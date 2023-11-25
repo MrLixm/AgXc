194089b9cf359b8a4fa066713a7aa167f4b8c7c5
Liam Collod <lcollod@gmail.com>
2023-11-25 21:56:01 +0100

    !refacto(obs): move AgX specific code to own module; change Outset UX
    had to remove the `Apply Outset` param to keep the code DRT agnostic, outset can be applied as an additional DRT instead

--- obs/obs-script/AgX.hlsl
+++ obs/obs-script/AgX.hlsl
@@ -1,5 +1,9 @@
 /*
-HLSL implementation of AgX (by Troy Sobotka) for OBS.
+HLSL implementation of AgX for OBS.
+
+AgX is a Display Rendering Transform designed by Troy Sobotka.
+
+The code is not AgX-specific and actually very flexible to integrate any other DRT.
 
 author = "Liam Collod"
 repository = "https://github.com/MrLixm/AgXc"
@@ -12,9 +16,6 @@ References:
 - [4] https://github.com/Unity-Technologies/Graphics/blob/master/com.unity.postprocessing/PostProcessing/Shaders/Colors.hlsl#L574
 - [5] https://github.com/colour-science/colour/blob/develop/colour/models/rgb/transfer_functions/srgb.py#L99
 */
-// order matters !!
-#include "lib_math.hlsl"
-#include "lib_colorscience.hlsl"
 
 // OBS-specific syntax adaptation to HLSL standard to avoid errors reported by the code editor
 #define SamplerState sampler_state
@@ -45,7 +46,6 @@ uniform float PUNCH_EXPOSURE = 0.0;
 uniform float PUNCH_SATURATION = 1.0;
 uniform float PUNCH_GAMMA = 1.0;
 uniform bool USE_OCIO_LOG = false;
-uniform bool APPLY_OUTSET = false;
 
 // LUT AgX-default_contrast.lut.png
 uniform texture2d AgXLUT;
@@ -58,6 +58,7 @@ uniform texture2d AgXLUT;
 
 uniform int drt_id_none = 0;
 uniform int drt_id_agx = 1;
+uniform int drt_id_agx_outset = 2;
 
 /*=================
     OBS BOILERPLATE
@@ -89,6 +90,10 @@ struct PixelData
     float2 uv  : TEXCOORD0; // UV coordinates in the source picture
 };
 
+// order matters + need the constants defined above
+#include "lib_math.hlsl"
+#include "lib_colorscience.hlsl"
+#include "lib_agx.hlsl"
 
 /*=================
     Main processes
@@ -118,95 +123,16 @@ float3 applyGrading(float3 Image)
     return Image;
 }
 
-float3 applyAgXLog(float3 Image)
-/*
-    Prepare the data for display encoding. Converted to log domain.
-*/
-{
-
-    float3x3 agx_compressed_matrix = {
-        0.84247906, 0.0784336, 0.07922375,
-        0.04232824, 0.87846864, 0.07916613,
-        0.04237565, 0.0784336, 0.87914297
-    };
-
-    Image = max(0.0, Image); // clamp negatives
-    // why this doesn't work ??
-    // Image = mul(agx_compressed_matrix, Image);
-	Image = apply_matrix(Image, agx_compressed_matrix);
-
-    if (USE_OCIO_LOG)
-        Image = cctf_log2_ocio_transform(Image);
-    else
-        Image = cctf_log2_normalized_from_open_domain(Image, -10.0, 6.5);
-
-    Image = clamp(Image, 0.0, 1.0);
-    return Image;
-}
-
-float3 applyAgXLUT(float3 Image)
-/*
-    Apply the AgX 1D curve on log encoded data.
-
-    The output is similar to AgX Base which is considered
-    sRGB - Display, but here we linearize it.
-
-    -- ref[3] for LUT implementation
-*/
-{
-
-    float3 lut3D = Image * (AgXLUT_BLOCK_SIZE-1);
-
-    float2 lut2D[2];
-    // Front
-    lut2D[0].x = floor(lut3D.z) * AgXLUT_BLOCK_SIZE+lut3D.x;
-    lut2D[0].y = lut3D.y;
-    // Back
-    lut2D[1].x = ceil(lut3D.z) * AgXLUT_BLOCK_SIZE+lut3D.x;
-    lut2D[1].y = lut3D.y;
-
-    // Convert from texel to texture coords
-    lut2D[0] = (lut2D[0]+0.5) * AgXLUT_PIXEL_SIZE;
-    lut2D[1] = (lut2D[1]+0.5) * AgXLUT_PIXEL_SIZE;
-
-    // Bicubic LUT interpolation
-    Image = lerp(
-        AgXLUT.Sample(LUTSampler, lut2D[0]).rgb, // Front Z
-        AgXLUT.Sample(LUTSampler, lut2D[1]).rgb, // Back Z
-        frac(lut3D.z)
-    );
-    // LUT apply the transfer function so we remove it to keep working on linear data.
-    Image = cctf_decoding_Power_2_2(Image);
-    return Image;
-}
-
-float3 applyOutset(float3 Image)
-/*
-    Outset is the inverse of the inset applied during `applyAgXLog`
-    and restore chroma.
-*/
-{
-
-    float3x3 agx_compressed_matrix_inverse = {
-        1.1968790, -0.09802088, -0.09902975,
-        -0.05289685, 1.15190313, -0.09896118,
-        -0.05297163, -0.09804345, 1.15107368
-    };
-	Image = apply_matrix(Image, agx_compressed_matrix_inverse);
-
-    return Image;
-}
-
 float3 applyDRT(float3 Image)
 /*
     Apply the DRT selected by the user.
 */
 {
     if (DRT == drt_id_agx){
-        Image = applyAgXLog(Image);
-        Image = applyAgXLUT(Image);
-        if (APPLY_OUTSET)
-            Image = applyOutset(Image);
+        Image = applyAgX(Image);
+    }
+    if (DRT == drt_id_agx_outset){
+        Image = applyAgXOutset(Image);
     }
     return Image;
 
