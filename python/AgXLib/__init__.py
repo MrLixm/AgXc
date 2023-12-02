5617344b0a28dc557b6c039899969b3ed3b9c8bd
Liam Collod <lcollod@gmail.com>
2023-12-02 16:58:31 +0100

    feat(python.AgXLib): add new log2lin function in interface

--- python/AgXLib/__init__.py
+++ python/AgXLib/__init__.py
@@ -1,6 +1,7 @@
 from .reshape import get_reshaped_colorspace_matrix
 from .tonescale import apply_AgX_tonescale
 from .cctf import convert_open_domain_to_normalized_log2
+from .cctf import convert_normalized_log2_to_open_domain
 from .apply import convert_imagery_to_AgX_closeddomain
 from . import grading
 
