237c7c31bcfc06151cbfe9644b054f5373dda406
Liam Collod <lcollod@gmail.com>
2023-12-02 13:09:54 +0100

    feat(python.AgXLib): add `__version__` var

--- python/AgXLib/__init__.py
+++ python/AgXLib/__init__.py
@@ -2,3 +2,5 @@ from .reshape import get_reshaped_colorspace_matrix
 from .tonescale import apply_AgX_tonescale
 from .tonescale import convert_open_domain_to_normalized_log2
 from .apply import convert_imagery_to_AgX_closeddomain
+
+__version__ = "0.1.0"
