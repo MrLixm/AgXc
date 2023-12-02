75ac524af340bbf384c790e654f9d34ddae980b7
Liam Collod <lcollod@gmail.com>
2023-12-02 17:42:23 +0100

    refacto(python.AgXLib): tonescale code style improvement

--- python/AgXLib/tonescale.py
+++ python/AgXLib/tonescale.py
@@ -1,9 +1,12 @@
 """
 Nearly all the content here is a direct copy of Troy Sobotka work :
 https://github.com/sobotka/AgX-S2O3/blob/main/AgX.py
+Which based himself on the work of Jed Smith.
+
 Slight modifications in terms of code style but outcome is similar.
 """
 import logging
+import typing
 
 import numpy
 
@@ -12,51 +15,61 @@ from ._types import Ndarray
 LOGGER = logging.getLogger(__name__)
 
 
+NdarrayT = typing.TypeVar("NdarrayT", bound=Ndarray)
+
+
 def _equation_scale(
-    x_pivot: Ndarray,
-    y_pivot: Ndarray,
-    slope_pivot: Ndarray,
-    power: Ndarray,
-) -> Ndarray:
+    x_pivot: NdarrayT,
+    y_pivot: NdarrayT,
+    slope_pivot: NdarrayT,
+    power: NdarrayT,
+) -> NdarrayT:
     """
-    The following is a completely tunable sigmoid function compliments
-    of the incredible hard work of Jed Smith. He's an incredible peep,
-    but don't let anyone know that I said that -- (Troy.S)
+    All argument expected to be of the same last dimension.
     """
     a = (slope_pivot * x_pivot) ** -power
     b = ((slope_pivot * (x_pivot / y_pivot)) ** power) - 1.0
     return (a * b) ** (-1.0 / power)
 
 
-def _equation_hyperbolic(x: Ndarray, power: Ndarray) -> Ndarray:
+def _equation_hyperbolic(x: NdarrayT, power: NdarrayT) -> NdarrayT:
+    """
+    All argument expected to be of the same last dimension.
+    """
     return x / ((1.0 + x**power) ** (1.0 / power))
 
 
 def _equation_term(
-    x: Ndarray,
-    x_pivot: Ndarray,
-    slope_pivot: Ndarray,
-    scale: Ndarray,
-) -> Ndarray:
-    return (slope_pivot * (x - x_pivot)) / scale
+    array: NdarrayT,
+    x_pivot: NdarrayT,
+    slope_pivot: NdarrayT,
+    scale: NdarrayT,
+) -> NdarrayT:
+    """
+    All argument expected to be of the same last dimension.
+    """
+    return (slope_pivot * (array - x_pivot)) / scale
 
 
 def _equation_curve(
-    x: Ndarray,
+    array: Ndarray,
     x_pivot: Ndarray,
     y_pivot: Ndarray,
     slope_pivot: Ndarray,
     power: Ndarray,
     scale: Ndarray,
 ) -> Ndarray:
+    """
+    All argument expected to be of the same last dimension.
+    """
     a = _equation_hyperbolic(
-        _equation_term(x, x_pivot, slope_pivot, scale), power[..., 0]
+        _equation_term(array, x_pivot, slope_pivot, scale), power[..., 0]
     )
     a *= scale
     a += y_pivot
 
     b = _equation_hyperbolic(
-        _equation_term(x, x_pivot, slope_pivot, scale), power[..., 1]
+        _equation_term(array, x_pivot, slope_pivot, scale), power[..., 1]
     )
     b *= scale
     b += y_pivot
@@ -66,19 +79,14 @@ def _equation_curve(
 
 
 def _equation_full_curve(
-    lut_array: Ndarray,
-    x_pivot: float,
-    y_pivot: float,
-    slope_pivot: float,
-    power: tuple[float, float],
+    array: Ndarray,
+    x_pivot: Ndarray,
+    y_pivot: Ndarray,
+    slope_pivot: Ndarray,
+    power: Ndarray,
 ) -> Ndarray:
-    x_pivot = numpy.asarray(x_pivot)
-    y_pivot = numpy.asarray(y_pivot)
-    slope_pivot = numpy.asarray(slope_pivot)
-    power = numpy.asarray(power)
-
-    scale_x_pivot = numpy.where(lut_array >= x_pivot, 1.0 - x_pivot, x_pivot)
-    scale_y_pivot = numpy.where(lut_array >= x_pivot, 1.0 - y_pivot, y_pivot)
+    scale_x_pivot = numpy.where(array >= x_pivot, 1.0 - x_pivot, x_pivot)
+    scale_y_pivot = numpy.where(array >= x_pivot, 1.0 - y_pivot, y_pivot)
 
     toe_scale = _equation_scale(
         scale_x_pivot, scale_y_pivot, slope_pivot, power[..., 0]
@@ -87,13 +95,13 @@ def _equation_full_curve(
         scale_x_pivot, scale_y_pivot, slope_pivot, power[..., 1]
     )
 
-    scale = numpy.where(lut_array >= x_pivot, shoulder_scale, -toe_scale)
+    scale = numpy.where(array >= x_pivot, shoulder_scale, -toe_scale)
 
-    return _equation_curve(lut_array, x_pivot, y_pivot, slope_pivot, power, scale)
+    return _equation_curve(array, x_pivot, y_pivot, slope_pivot, power, scale)
 
 
 def apply_AgX_tonescale(
-    rgbarray: Ndarray,
+    array: Ndarray,
     min_EV: float = -10.0,
     max_EV: float = +6.5,
     general_contrast: float = 2.0,
@@ -103,11 +111,13 @@ def apply_AgX_tonescale(
     Apply the AgX 1D tonescale curve on the given R-G-B array.
 
     Args:
-        rgbarray: R-G-B imagery data, usually in a "shaper space" like log encoding.
-        min_EV: minimal exposure fitted in the [0,1] range.
-        max_EV: maximum exposure fitted in the [0,1] range.
-        general_contrast:
-        limits_contrast:
+        array:
+            imagery data as RGB or single channel,
+            usually in a "shaper space" like log encoding.
+        min_EV: minimal exposure being fitted in the curve [0,1] range.
+        max_EV: maximum exposure being fitted in the curve [0,1] range.
+        general_contrast: increase "s" shape
+        limits_contrast: toe and shoulder contrast
 
     Returns:
         new array with the tonescale applied, same shape as input array.
@@ -115,8 +125,13 @@ def apply_AgX_tonescale(
     AgX_x_pivot = numpy.abs(min_EV / (max_EV - min_EV))
     AgX_y_pivot = 0.50
 
+    AgX_x_pivot = numpy.asarray(AgX_x_pivot)
+    AgX_y_pivot = numpy.asarray(AgX_y_pivot)
+    general_contrast = numpy.asarray(general_contrast)
+    limits_contrast = numpy.asarray(limits_contrast)
+
     converted = _equation_full_curve(
-        rgbarray,
+        array,
         AgX_x_pivot,
         AgX_y_pivot,
         general_contrast,
