237572bafc67186cefac67ea688b9ac1aaa15a59
Liam Collod <lcollod@gmail.com>
2025-02-25 20:15:27 +0100

    chore(tests.ocio.nuke): newer nuke version; rename test

--- .dev/implementations/ocio/tests/nuke/test-nuke.sh
+++ .dev/implementations/ocio/tests/nuke/test-nuke.sh
@@ -2,9 +2,9 @@ THISDIR=$(dirname "$0")
 cd "$THISDIR" || exit
 
 ocio_path="$THISDIR/../../../../../ocio/AgXc_default_OCIO-v1/config.ocio"
-"C:\Program Files\Nuke15.0v2\Nuke15.0.exe" --nc -t ./test_config.py "$ocio_path"
+"C:\Program Files\Nuke15.1v5\Nuke15.1.exe" --nc -t ./test_config.py "$ocio_path"
 echo "------------------------------------------------------------------------"
 
 ocio_path="$THISDIR/../../../../../ocio/AgXc_default_OCIO-v2/config.ocio"
-"C:\Program Files\Nuke15.0v2\Nuke15.0.exe" --nc -t ./test_config.py "$ocio_path"
+"C:\Program Files\Nuke15.1v5\Nuke15.1.exe" --nc -t ./test_config.py "$ocio_path"
 echo "------------------------------------------------------------------------"
